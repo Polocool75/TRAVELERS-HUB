f9cb6b6148e6a43a01316f73c789f5a8095061ef
picolo16 <mateo.touron@edu.devinci.fr>
2021-05-17 11:15:43 +0200

    MaJ
    Mise en place du systeme pour modifier directement dans le fichier Github

---
+++ TRAVELERS_V1/TRAVELERS_V1.ino
@@ -0,0 +1,150 @@
+#include <PS4Controller.h>
+
+const int dead = 50; // Agrandissement de la zone morte 
+
+//Pour les moteur, la numérotation suit ce schéma
+/* 
+ * AVANT
+ * 1    4
+ * 2    5
+ * 3    6
+ *  
+ */
+const int Motor1A = 12;
+const int Motor1R = 13;
+
+const int Motor2A = 14;
+const int Motor2R = 27;
+
+const int Motor3A = 26;
+const int Motor3R = 25;
+
+const int Motor4A = 33;
+const int Motor4R = 32;
+
+const int Motor5A = 15;
+const int Motor5R = 4;
+
+const int Motor6A = 19;
+const int Motor6R = 5;
+
+const int freq = 500; //fq du PWM
+const int resolution = 8; Resolution (8 octets = 256 valeurs prenables pour le PWM
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+  PS4.begin("01:01:01:01:01:01");
+
+  for(int i= 1; i<=12;i++)
+  {
+     ledcSetup(i,freq, resolution); //Le channel 0 aura donc comme fq 5000hz et 8 de résolution
+  }
+  
+  ledcAttachPin(Motor1A,1);
+  ledcAttachPin(Motor1R,2);
+  ledcAttachPin(Motor2A,3);
+  ledcAttachPin(Motor2R,4); 
+  ledcAttachPin(Motor3R,5);
+  ledcAttachPin(Motor3A,6);
+  ledcAttachPin(Motor4A,7);
+  ledcAttachPin(Motor4R,8);
+  ledcAttachPin(Motor5A,9);
+  ledcAttachPin(Motor5R,10);
+  ledcAttachPin(Motor6A,11); 
+  ledcAttachPin(Motor6R,12);
+}
+
+void Move(int gauche, int droite, bool L_A, bool R_A)
+{
+  if (PS4.isConnected()) 
+  {
+  //Une ou deux roues en mouvement
+  if((gauche<=-dead || gauche >= dead) || (droite<= -dead ||droite >= dead))
+  {
+    int valueR = map(abs(droite),0,129,0,255);
+    int valueL= map(abs(gauche),0,129,0,255);
+    Serial.println(valueR);
+    Serial.println(valueL);
+    
+    if(L_A)
+     {
+        ledcWrite(1,valueL);
+        ledcWrite(2,0);
+        ledcWrite(3,valueL);
+        ledcWrite(4,0);
+        ledcWrite(5,valueL);
+        ledcWrite(6,0);
+     }
+     else
+     {
+        ledcWrite(1,0);
+        ledcWrite(2,valueL);
+        ledcWrite(3,0);
+        ledcWrite(4,valueL);
+        ledcWrite(5,0);
+        ledcWrite(6,valueL);
+     }
+
+     if(R_A)
+     {
+        ledcWrite(7,valueR);
+        ledcWrite(8,0);
+        ledcWrite(9,valueR);
+        ledcWrite(10,0);
+        ledcWrite(11,valueR);
+        ledcWrite(12,0);
+     }
+     else
+     {
+        ledcWrite(7,0);
+        ledcWrite(8,valueL);
+        ledcWrite(9,0);
+        ledcWrite(10,valueL);
+        ledcWrite(11,0);
+        ledcWrite(12,valueL);
+     }
+  }
+  else
+  {
+    ledcWrite(1,0);
+    ledcWrite(2,0);
+    ledcWrite(3,0);
+    ledcWrite(4,0);
+    ledcWrite(5,0);
+    ledcWrite(6,0);
+    ledcWrite(7,0);
+    ledcWrite(8,0);
+    ledcWrite(9,0);
+    ledcWrite(10,0);
+    ledcWrite(11,0);
+    ledcWrite(12,0);
+  }
+}
+}
+
+void loop() {
+if (PS4.isConnected()) 
+{
+    int RStick = PS4.RStickY();
+    bool R_A = PS4.RStickY()>0;
+    int LStick = PS4.LStickY();
+    bool L_A = PS4.LStickY()>0;
+    
+    if(RStick<=-dead || RStick >= dead)
+    {
+      Serial.println(RStick);
+      Serial.println(R_A);
+    }
+    if(LStick<=-dead || LStick >= dead)
+    {
+      Serial.println(LStick);
+      Serial.println(L_A);
+    }
+    
+    Move(LStick,RStick,L_A,R_A);
+    
+    delay(50);
+}
+}
