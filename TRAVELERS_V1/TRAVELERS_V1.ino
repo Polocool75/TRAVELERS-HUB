358772a7e87e35f9f7796f3119188a39d106090e
picolo16 <mateo.touron@edu.devinci.fr>
2021-06-14 17:07:43 +0200

    Update TRAVELERS_V1.ino

--- TRAVELERS_V1/TRAVELERS_V1.ino
+++ TRAVELERS_V1/TRAVELERS_V1.ino
@@ -1,26 +1,29 @@
-#include <PS4Controller.h>
+#include <PS4Controller.h> 
 
+// Agrandissement de la zone morte des joysticks (plus la zone morte est grande plus le joystick devra être décalé de son centre pour détecter une variation):
+const int dead = 40; 
 
-const int dead = 40; // Agrandissement de la zone morte
+// Intensité de rouge, vert et bleu de la lumière de la manette PS4:
 int r = 50;
 int g = 50;
 int b = 50;
 
+// Les coefficients de vitesse des roues Avant, Milieu et aRrière:
 float coefA = 1;
 float coefM = 1;
 float coefR = 1;
 
-int slow = 0;
+// Indicateur (booléen) si la touche carré, triangle ou cercle a été appuyé:
 bool carre = false;
 bool triangle = false;
 bool cercle = false;
 
-/* Pour les moteur, la numérotation suit ce schéma
+/* Pour les moteur, la numérotation suit ce schéma (vu du dessus):
 
      AVANT
-    1    4
-    2    5
-    3    6
+    1     4
+    2     5
+    3     6
 
 */
 
@@ -43,17 +46,24 @@ const int Motor5R = 27;
 const int Motor6A = 33;
 const int Motor6R = 32;
 
-const int freq = 500; // Fq du PWM
-const int resolution = 8; // Resolution (8 octets = 256 valeurs prenables pour le PWM)
+const int freq = 500; // Fréquence du PWM
+const int resolution = 8; // Resolution (8 octets = 256 valeurs prenables pour le PWM, de 0 à 255)
 
 void setup()
 {
-  Serial.begin(115200);
-  PS4.begin("02:02:02:02:02:02");
+  Serial.begin(115200); //Pour quand il y a un problème
+  PS4.begin("02:02:02:02:02:02"); //Lance la connection à la manette PS4 (le paramètre est l'adresse MAC de la manette)
   for (int i = 1; i <= 12; i++)
   {
-    ledcSetup(i, freq, resolution); //Tous les channels auront donc comme fq 5000hz et 8 de résolution
-    //nb:
+    /* Fonctionnement du PWM avec l'ESP32:
+     * Tout d'abord créer un "programme pré-enregistré" avec ledcSetup([numéro du programme (1 à 16)],[fq du PWM], [résolution du PWM])
+     * Ensuite, on "attache" un pin à un programme avec ledcAttachPin([pin],[numéro du programme])
+     * Attention, on ne peut pas attacher plusieurs pins à un même programme (ici, on a donc du créer 12 programmes différents ayant les mêmes caractéristiques)
+     * 
+     * Enfin, pour utiliser le PWM, on utiliser ledcWrite([numéro du programme],[valeur du PWM])
+     * 
+     */
+    ledcSetup(i, freq, resolution); // Tous les programmes/channels de 1 à 12 auront donc comme fq 500hz et 8 de résolution
   }
 
   ledcAttachPin(Motor1A, 1);
@@ -70,12 +80,54 @@ void setup()
   ledcAttachPin(Motor6R, 12);
 }
 
+void loop() {
+  if (PS4.isConnected())// Le code ne se lance que si la manette PS4 est connecté
+  {
+    int RStick = PS4.RStickY(); // Valeur de l'intensité d'inclinaison du joystick droit (entre 127 et -128)
+    bool R_A = PS4.RStickY() > 0; // Booléen indiquant si l'on veut avancer ou non (pour le joystick droit)
+    int LStick = PS4.LStickY(); // idem pour joystick gauche
+    bool L_A = PS4.LStickY() > 0; //idem pour joystick gauche
+    
+    if (PS4.Square()) //Si le bouton carré est pressé
+    {
+      carre = !carre;
+      b = carre ? 210 : 50; //La valeur d'intensité passe à 210 si carre est pressé et 50 si le bouton avait déjà été pressé et qu'on represse
+      delay(100);
+    }
+    if (PS4.Circle())//Idem avec cercle
+    {
+      cercle = !cercle;
+      r = cercle ? 210 : 50;
+      delay(100);
+    }
+    if (PS4.Triangle())//Idem avec triangle
+    {
+      triangle = !triangle;
+      g = triangle ? 210 : 50;
+      delay(100);
+    }
+    if (triangle && cercle && carre) // Si les 3 boutons sont appuyés cela veut dire qu'on reduit la vitesse de toutes les roues : c'est inutile donc on annule
+    {
+      triangle = false;
+      carre = false;
+      cercle = false;
+      r = 50;
+      g = 50;
+      b = 50;
+    }
+    //Pour indiquer le mode, on utilise la lumière de la manette PS4
+    PS4.setLed(r, g, b);
+    PS4.sendToController();
+    Move(LStick, RStick, L_A, R_A);
+    delay(50);
+  }
+}
+
 void Move(int gauche, int droite, bool L_A, bool R_A)
 {
   if (PS4.isConnected())
   {
-
-    if ((gauche <= -dead || gauche >= dead) || (droite <= -dead || droite >= dead))
+    if ((gauche <= -dead || gauche >= dead) || (droite <= -dead || droite >= dead))//
     {
       coefA = 1;
       coefM = 1;
@@ -85,10 +137,7 @@ void Move(int gauche, int droite, bool L_A, bool R_A)
         coefM = triangle ? 1 : 0.80 ;
         coefR = carre ? 1 : 0.80 ;
       }
-      Serial.println(coefA);
-      Serial.println(coefM);
-      Serial.println(coefR);
-
+      
       int valueR = 0;
       int valueL = 0;
       if ((droite <= -dead || droite >= dead))
@@ -149,49 +198,5 @@ void Move(int gauche, int droite, bool L_A, bool R_A)
       ledcWrite(11, 0);
       ledcWrite(12, 0);
     }
-  }
-}
-
-void loop() {
-  if (PS4.isConnected())
-  {
-    int RStick = PS4.RStickY();
-    bool R_A = PS4.RStickY() > 0;
-    int LStick = PS4.LStickY();
-    bool L_A = PS4.LStickY() > 0;
-    if (PS4.Square())
-    {
-      carre = !carre;
-      b = carre ? 210 : 50;
-      delay(100);
-    }
-    if (PS4.Circle())
-    {
-      cercle = !cercle;
-      r = cercle ? 210 : 50;
-      delay(100);
-    }
-    if (PS4.Triangle())
-    {
-      triangle = !triangle;
-      g = triangle ? 210 : 50;
-      delay(100);
-    }
-    if (triangle && cercle && carre)
-    {
-      triangle = false;
-      carre = false;
-      cercle = false;
-      r = 50;
-      g = 50;
-      b = 50;
-    }
-    PS4.setLed(r, g, b);
-    PS4.sendToController();
-    Move(LStick, RStick, L_A, R_A);
-    delay(50);
-  }
-  else{
-    Serial.println("test");
   }
 }
