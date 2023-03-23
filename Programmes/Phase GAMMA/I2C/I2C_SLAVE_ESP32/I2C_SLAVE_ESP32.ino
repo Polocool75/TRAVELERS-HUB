8caefd35c9b3ad1696c0385125e4b5b78981b13d
picolo16 <mateo.touron@edu.devinci.fr>
2023-03-23 16:52:01 +0100

    MaJ I2C

--- Programmes/Phase GAMMA/I2C/I2C_SLAVE_ESP32/I2C_SLAVE_ESP32.ino
+++ Programmes/Phase GAMMA/I2C/I2C_SLAVE_ESP32/I2C_SLAVE_ESP32.ino
@@ -2,48 +2,216 @@
 
 #define I2C_DEV_ADDR 0x11
 
-uint32_t i = 0;
+/* Structure des messages I2C (String):
+    "[1],[2],[3],[4]"
+
+   Pour se déplacer Terrestre / Aquatique:
+    [1] : "M" / "A" -> Terrestre / Aquatique
+    [2] : int() -> Valeurs du PWM ([0,255])
+    [3] : "L" ou "R" -> Moteurs gauche (L) ou droit (R)
+    [4] : "A" ou "R" -> Sens avant (A) ou arrière (R)
+
+   Boutons :
+    [1] : "B" -> Boutons
+    [2] : "1", "2", "3", "4", "5" -> Carré, Triangle, Rond, Croix, Start
+
+*/
+
+const int freq = 500;             // Fréquence du PWM
+const int resolution = 8;        // Resolution (8 octets = 256 valeurs prenables pour le PWM, de 0 à 255)
+
+const int Motor1A = 23;
+const int Motor1R = 17;
+
+const int Motor2A = 5;
+const int Motor2R = 19;
+
+const int Motor3A = 27;
+const int Motor3R = 13;
+
+const int Motor4A = 25;
+const int Motor4R = 26;
+
+const int enable = 2;
+bool carre = false;
+
 volatile boolean receiveFlag = false;
 char temp[32];
-String command;
+int pwm;
+String wheel, dir;
 
-void onRequest(){
-  Wire.print(i++);
-  Wire.print(" Packets.");
-  Serial.println("onRequest");
+void onRequest() {
 }
 
-void onReceive(int len){
-  for (int j = 0; j<len; j++)
+void onReceive(int len) {
+  for (int j = 0; j < len; j++)
   {
-    temp[j]=Wire.read();
-    temp[j+1] = '\0';
+    temp[j] = Wire.read();
+    temp[j + 1] = '\0';
   }
-  for (int j=0; j<len; j++)
+  for (int j = 0; j < len; j++)
   {
-    temp[j] = temp[j+1];
+    temp[j] = temp[j + 1];
   }
   receiveFlag = true;
 
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial.setDebugOutput(true);
+void setup()
+{
   Wire.onReceive(onReceive);
-  Wire.onRequest(onRequest);
+  Wire.onRequest(onRequest); // On mettra la les données de l'odo
   Wire.begin((uint8_t)I2C_DEV_ADDR);
 
+  pinMode(Motor1A, OUTPUT);
+  pinMode(Motor1R, OUTPUT);
+  pinMode(Motor2A, OUTPUT);
+  pinMode(Motor2R, OUTPUT);
+  pinMode(Motor3A, OUTPUT);
+  pinMode(Motor3R, OUTPUT);
+  pinMode(Motor4A, OUTPUT);
+  pinMode(Motor4R, OUTPUT);
+  pinMode(enable, OUTPUT);
+
+  digitalWrite(enable, LOW);
+
+  for (int i = 1; i <= 8; i++)
+  {
+    /* Fonctionnement du PWM avec l'ESP32:
+       Tout d'abord créer un "programme pré-enregistré" avec ledcSetup([numéro du programme (1 à 16)],[fq du PWM], [résolution du PWM])
+       Ensuite, on "attache" un pin à un programme avec ledcAttachPin([pin],[numéro du programme])
+       Attention, on ne peut pas attacher plusieurs pins à un même programme (ici, on a donc du créer 12 programmes différents ayant les mêmes caractéristiques)
+
+       Enfin, pour utiliser le PWM, on utiliser ledcWrite([numéro du programme],[valeur du PWM])
+
+    */
+    ledcSetup(i, freq, resolution);                    // Tous les programmes/channels de 1 à 12 auront donc comme fq 500hz et 8 de résolution
+  }
+
+  ledcAttachPin(Motor1A, 1);
+  ledcAttachPin(Motor1R, 2);
+  ledcAttachPin(Motor2A, 3);
+  ledcAttachPin(Motor2R, 4);
+
+  ledcAttachPin(Motor3A, 5);
+  ledcAttachPin(Motor3R, 6);
+  ledcAttachPin(Motor4A, 7);
+  ledcAttachPin(Motor4R, 8);
 
 }
 
-void loop() {
-  delay(300);
-  if(receiveFlag == true)
+void MoveValues(String input_str, int& val1, String& val2, String& val3) {
+  int index = 2; // initialise l'index à 2 pour pas compter M
+  String temp_str = ""; // initialise la chaîne temporaire
+  int count = 0; // initialise le compteur de valeurs
+  while (index < input_str.length()) { // tant qu'il y a des caractères dans la chaîne
+    char current_char = input_str.charAt(index); // récupère le caractère actuel
+    if (current_char == ',') { // si c'est une virgule
+      count++; // incrémente le compteur de valeurs
+      if (count == 1) { // si c'est la première valeur
+        val1 = temp_str.toInt(); // convertit la chaîne en entier et l'assigne à la première valeur
+      } else if (count == 2) { // si c'est la deuxième valeur
+        val2 = String(temp_str); // convertit la chaîne en entier et l'assigne à la deuxième valeur
+      }
+      temp_str = ""; // réinitialise la chaîne temporaire
+    } else { // si ce n'est pas une virgule
+      temp_str += current_char; // ajoute le caractère à la chaîne temporaire
+    }
+    index++; // incrémente l'index
+  }
+  if (input_str.length() > 3) // Sinon c'est qu'on est a pwm = 0 ou Motor Aqua
+    val3 = String(temp_str);
+  temp_str = "";
+
+}
+
+void ButtonAction(String input_str)
+{
+  int index = 2; // initialise l'index
+  String button_str = "";
+  int button;
+  while (index < input_str.length())
   {
-    Serial.println(temp);
+    button_str += input_str.charAt(index);
+    index++;
+  }
+  button = button_str.toInt();
+  switch (button)
+  {
+    case 1: // Carré
+      carre = !carre;
+      if (carre)
+        digitalWrite(enable, HIGH);
+      else
+        digitalWrite(enable, LOW);
+      break;
+
+    case 2: // Triangle
+      break;
+
+    case 3: // Rond
+      break;
+
+    case 4: // Croix
+      break;
+
+    case 5: // Start
+      break;
+
+    default:
+      break;
+  }
+}
+
+void loop() {
+  delay(100);
+  if (receiveFlag == true)
+  {
+    String mess = String(temp);
+    if (mess.charAt(0) == 'M' || mess.charAt(0) == 'A')
+    {
+      MoveValues(mess, pwm, wheel, dir);
+      if (wheel == "L") // Roue de gauche
+      {
+        if (dir == "A") // Avance
+        {
+          ledcWrite(1, round(pwm));
+          ledcWrite(2, 0);
+          ledcWrite(5, round(pwm));
+          ledcWrite(6, 0);
+        }
+        else // Recule
+        {
+          ledcWrite(1, 0);
+          ledcWrite(2, round(pwm));
+          ledcWrite(5, 0);
+          ledcWrite(6, round(pwm));
+        }
+      }
+      else
+      {
+        if (dir == "A") // Avance
+        {
+          ledcWrite(3, round(pwm));
+          ledcWrite(4, 0);
+          ledcWrite(7, round(pwm));
+          ledcWrite(8, 0);
+        }
+        else // Recule
+        {
+          ledcWrite(3, 0);
+          ledcWrite(4, round(pwm));
+          ledcWrite(7, 0);
+          ledcWrite(8, round(pwm));
+        }
+      }
+
+      //On sait dans quelle direction, sur quelle roue et a quelle puissance on va
+    }
+    else
+    {
+      ButtonAction(mess);
+    }
     receiveFlag = false;
   }
-  
-
 }
--- Programmes/Phase GAMMA/PS4_BT_WIFI_SEND.py
+++ Programmes/Phase GAMMA/PS4_BT_WIFI_SEND.py
@@ -1,7 +1,23 @@
 from pyPS4Controller.controller import Controller
 import socket
 import threading
+import time
 
+UDP_IP = "172.21.72.182" # "127.0.0.1" = loopback (pour ce l'envoyer à nous même), par ailleurs on ne peut pas broadcast
+UDP_PORT = 5005
+MESSAGE = "1,2,3"
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+MAX = 32767
+dead = 30
+delay = 100*(10)**(-6)
+
+# Structure des messages : 
+# [valuePWM,L ou R,A ou R]
+def Mapping(val, oldMax = MAX, newMax = 255) :
+    return int(val * (newMax/oldMax))
 
 class MyController(Controller):
 
@@ -9,58 +25,33 @@ class MyController(Controller):
         Controller.__init__(self, **kwargs)
 
     def on_x_press(self):
-        print("on_x_press")
-
-    def on_x_release(self):
-        pass
+        MESSAGE = "B,1"
+        sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        time.sleep(delay)
 
     def on_triangle_press(self):
         print("on_triangle_press")
 
-    def on_triangle_release(self):
-        pass
-
     def on_circle_press(self):
         print("on_circle_press")
-
-    def on_circle_release(self):
-        pass 
 
     def on_square_press(self):
         print("on_square_press")
 
-    def on_square_release(self):
-        pass
-
     def on_L1_press(self):
         print("on_L1_press")
-
-    def on_L1_release(self):
-        pass
 
     def on_L2_press(self, value):
         print("on_L2_press: {}".format(value))
 
-    def on_L2_release(self):
-        pass
-
     def on_R1_press(self):
         print("on_R1_press")
-
-    def on_R1_release(self):
-        pass
 
     def on_R2_press(self, value):
         print("on_R2_press: {}".format(value))
 
-    def on_R2_release(self):
-        pass
-
     def on_up_arrow_press(self):
         print("on_up_arrow_press")
-
-    def on_up_down_arrow_release(self):
-        pass
 
     def on_down_arrow_press(self):
         print("on_down_arrow_press")
@@ -68,76 +59,61 @@ class MyController(Controller):
     def on_left_arrow_press(self):
         print("on_left_arrow_press")
 
-    def on_left_right_arrow_release(self):
-        pass
-
     def on_right_arrow_press(self):
         print("on_right_arrow_press")
 
     def on_L3_up(self, value):
-        print("on_L3_up: {}".format(value))
-
+        value = abs(Mapping(value,MAX))
+        if value >= dead :
+            MESSAGE = str("M") +str(value) + "," + str("L") + "," + str("A")
+            sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        time.sleep(delay)
+            
     def on_L3_down(self, value):
-        print("on_L3_down: {}".format(value))
-
-    def on_L3_left(self, value):
-        pass
-
-    def on_L3_right(self, value):
-        pass
+        value = Mapping(value,MAX)
+        if value >= dead :
+            MESSAGE = str("M") + str(value) + "," + str("L") + "," + str("R")
+            sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        time.sleep(delay)
 
     def on_L3_y_at_rest(self):
-        # mettre la valeur à 0
-        pass
+        MESSAGE = str("M") +str(0) + "," + str("L")
+        sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
 
-    def on_L3_x_at_rest(self):
-        pass
+
 
     def on_L3_press(self):
         """L3 joystick is clicked. This event is only detected when connecting without ds4drv"""
         print("on_L3_press")
 
-    def on_L3_release(self):
-        pass
-
     def on_R3_up(self, value):
-        print("on_R3_up: {}".format(value))
+        value = abs(Mapping(value,MAX))
+        if value >= dead :
+            MESSAGE = str("M") +str(value) + "," + str("R") + "," + str("A")
+            sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        time.sleep(delay)
 
     def on_R3_down(self, value):
-        print("on_R3_down: {}".format(value))
-
-    def on_R3_left(self, value):
-        pass
-
-    def on_R3_right(self, value):
-        pass
+        value = Mapping(value,MAX)
+        if value >= dead :
+            MESSAGE = str("M") +str(value) + "," + str("R") + "," + str("R")
+            sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        time.sleep(delay)
 
     def on_R3_y_at_rest(self):
-        # mettre la valeur à 0 
-        pass
-
-    def on_R3_x_at_rest(self):
-        pass
+        MESSAGE = str("M") +str(0) + "," + str("R")
+        sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT)) 
 
     def on_R3_press(self):
         """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
         print("on_R3_press")
 
-    def on_R3_release(self):
-        pass
-
     def on_options_press(self):
         print("on_options_press")
-
-    def on_options_release(self):
-        pass
 
     def on_share_press(self):
         """this event is only detected when connecting without ds4drv"""
         print("on_share_press")
-
-    def on_share_release(self):
-        pass
 
     def on_playstation_button_press(self):
         """this event is only detected when connecting without ds4drv"""
@@ -146,9 +122,68 @@ class MyController(Controller):
     def on_playstation_button_release(self):
         """this event is only detected when connecting without ds4drv"""
         print("on_playstation_button_release")
+     
 
-    
+    #Fonctions un peu inutiles
+    def on_x_release(self):
+        pass
 
+    def on_triangle_release(self):
+        pass
+
+    def on_circle_release(self):
+        pass 
+
+    def on_square_release(self):
+        pass
+
+    def on_L1_release(self):
+        pass
+
+    def on_L2_release(self):
+        pass
+
+    def on_R2_release(self):
+        pass
+
+    def on_R1_release(self):
+        pass
+
+    def on_up_down_arrow_release(self):
+        pass
+
+    def on_left_right_arrow_release(self):
+        pass
+
+    def on_L3_left(self, value):
+        pass
+
+    def on_L3_right(self, value):
+        pass
+
+    def on_L3_x_at_rest(self):
+        pass
+
+    def on_L3_release(self):
+        pass
+
+    def on_R3_left(self, value):
+        pass
+
+    def on_R3_right(self, value):
+        pass
+
+    def on_R3_x_at_rest(self):
+        pass
+
+    def on_options_release(self):
+        pass
+
+    def on_R3_release(self):
+        pass
+
+    def on_share_release(self):
+        pass
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
